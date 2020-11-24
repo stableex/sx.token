9a069e9372e9651de375e0eef59174e3626738bb
Denis <carriere.denis@gmail.com>
2020-11-24 16:10:56 -0500

    allow 0 max supply

--- token.sx.cpp
+++ token.sx.cpp
@@ -8,7 +8,7 @@ void token::create( const name&   issuer,
     auto sym = maximum_supply.symbol;
     check( sym.is_valid(), "invalid symbol name" );
     check( maximum_supply.is_valid(), "invalid supply");
-    check( maximum_supply.amount > 0, "max-supply must be positive");
+    check( maximum_supply.amount >= 0, "max-supply must be positive");
 
     stats statstable( get_self(), sym.code().raw() );
     auto existing = statstable.find( sym.code().raw() );
