e44ab4a4404644bac9c536f770bb1738b82687fa
Denis <carriere.denis@gmail.com>
2020-11-27 16:05:40 -0500

    refactor to eosio.token

--- token.sx.cpp
+++ token.sx.cpp
@@ -8,19 +8,18 @@ void token::create( const name&   issuer,
     auto sym = maximum_supply.symbol;
     check( sym.is_valid(), "invalid symbol name" );
     check( maximum_supply.is_valid(), "invalid supply");
-    check( maximum_supply.amount >= 0, "max-supply must be positive");
+    check( maximum_supply.amount > 0, "max-supply must be positive");
 
     stats statstable( get_self(), sym.code().raw() );
     auto existing = statstable.find( sym.code().raw() );
     check( existing == statstable.end(), "token with symbol already exists" );
 
     statstable.emplace( get_self(), [&]( auto& s ) {
-        s.supply.symbol = maximum_supply.symbol;
-        s.max_supply    = maximum_supply;
-        s.issuer        = issuer;
+       s.supply.symbol = maximum_supply.symbol;
+       s.max_supply    = maximum_supply;
+       s.issuer        = issuer;
     });
 }
-
 
 void token::issue( const name& to, const asset& quantity, const string& memo )
 {
@@ -39,10 +38,10 @@ void token::issue( const name& to, const asset& quantity, const string& memo )
     check( quantity.amount > 0, "must issue positive quantity" );
 
     check( quantity.symbol == st.supply.symbol, "symbol precision mismatch" );
+    check( quantity.amount <= st.max_supply.amount - st.supply.amount, "quantity exceeds available supply");
 
     statstable.modify( st, same_payer, [&]( auto& s ) {
-        s.supply += quantity;
-        s.max_supply += quantity;
+       s.supply += quantity;
     });
 
     add_balance( st.issuer, quantity, st.issuer );
@@ -62,11 +61,11 @@ void token::retire( const asset& quantity, const string& memo )
     require_auth( st.issuer );
     check( quantity.is_valid(), "invalid quantity" );
     check( quantity.amount > 0, "must retire positive quantity" );
+
     check( quantity.symbol == st.supply.symbol, "symbol precision mismatch" );
 
     statstable.modify( st, same_payer, [&]( auto& s ) {
-        s.supply -= quantity;
-        s.max_supply -= quantity;
+       s.supply -= quantity;
     });
 
     sub_balance( st.issuer, quantity );
@@ -99,12 +98,12 @@ void token::transfer( const name&    from,
 }
 
 void token::sub_balance( const name& owner, const asset& value ) {
-   accounts from_acnts( get_self(), owner.value );
+    accounts from_acnts( get_self(), owner.value );
 
-   const auto& from = from_acnts.get( value.symbol.code().raw(), "no balance object found" );
-   check( from.balance.amount >= value.amount, "overdrawn balance" );
+    const auto& from = from_acnts.get( value.symbol.code().raw(), "no balance object found" );
+    check( from.balance.amount >= value.amount, "overdrawn balance" );
 
-   from_acnts.modify( from, owner, [&]( auto& a ) {
+    from_acnts.modify( from, owner, [&]( auto& a ) {
         a.balance -= value;
     });
 }
@@ -113,7 +112,7 @@ void token::add_balance( const name& owner, const asset& value, const name& ram_
 {
     accounts to_acnts( get_self(), owner.value );
     auto to = to_acnts.find( value.symbol.code().raw() );
-    if( to == to_acnts.end() ) {
+    if ( to == to_acnts.end() ) {
         to_acnts.emplace( ram_payer, [&]( auto& a ){
             a.balance = value;
         });
@@ -126,18 +125,18 @@ void token::add_balance( const name& owner, const asset& value, const name& ram_
 
 void token::open( const name& owner, const symbol& symbol, const name& ram_payer )
 {
-   require_auth( ram_payer );
+    require_auth( ram_payer );
 
-   check( is_account( owner ), "owner account does not exist" );
+    check( is_account( owner ), "owner account does not exist" );
 
-   auto sym_code_raw = symbol.code().raw();
-   stats statstable( get_self(), sym_code_raw );
-   const auto& st = statstable.get( sym_code_raw, "symbol does not exist" );
-   check( st.supply.symbol == symbol, "symbol precision mismatch" );
+    auto sym_code_raw = symbol.code().raw();
+    stats statstable( get_self(), sym_code_raw );
+    const auto& st = statstable.get( sym_code_raw, "symbol does not exist" );
+    check( st.supply.symbol == symbol, "symbol precision mismatch" );
 
     accounts acnts( get_self(), owner.value );
     auto it = acnts.find( sym_code_raw );
-    if( it == acnts.end() ) {
+    if ( it == acnts.end() ) {
         acnts.emplace( ram_payer, [&]( auto& a ){
             a.balance = asset{0, symbol};
         });
@@ -154,16 +153,27 @@ void token::close( const name& owner, const symbol& symbol )
     acnts.erase( it );
 }
 
-void token::setissuer( const name issuer, const symbol_code symcode )
-{
-    stats statstable( get_self(), symcode.raw() );
-    auto itr = statstable.find( symcode.raw() );
+// void token::setissuer( const name issuer, const symbol_code symcode )
+// {
+//     stats statstable( get_self(), symcode.raw() );
+//     auto& stats = statstable.get( symcode.raw(), "symbol code does not exist");
 
-    check( is_account( issuer ), "issuer does not exist" );
-    check( itr != statstable.end(), "symbol code does not exist" );
-    check( has_auth( itr->issuer ), "missing current issuer authority");
+//     check( is_account( issuer ), "issuer does not exist" );
+//     check( has_auth( stats.issuer ), "missing current issuer authority");
 
-    statstable.modify( itr, get_self(), [&]( auto& row ) {
-        row.issuer = issuer;
-    });
-}
+//     statstable.modify( stats, get_self(), [&]( auto& row ) {
+//         row.issuer = issuer;
+//     });
+// }
+
+// void token::setmaxsupply( const symbol_code symcode, const int64_t amount )
+// {
+//     stats statstable( get_self(), symcode.raw() );
+//     auto& stats = statstable.get( symcode.raw(), "symbol code does not exist");
+
+//     check( has_auth( stats.issuer ), "missing current issuer authority");
+
+//     statstable.modify( stats, get_self(), [&]( auto& row ) {
+//         row.max_supply.amount = amount;
+//     });
+// }
--- token.sx.hpp
+++ token.sx.hpp
@@ -91,8 +91,11 @@ class [[eosio::contract("token.sx")]] token : public contract {
         [[eosio::action]]
         void close( const name& owner, const symbol& symbol );
 
-        [[eosio::action]]
-        void setissuer( const name issuer, const symbol_code symcode );
+        // [[eosio::action]]
+        // void setissuer( const name issuer, const symbol_code symcode );
+
+        // [[eosio::action]]
+        // void setmaxsupply( const symbol_code symcode, const int64_t amount );
 
         static asset get_supply( const name& token_contract_account, const symbol_code& sym_code )
         {
@@ -114,7 +117,6 @@ class [[eosio::contract("token.sx")]] token : public contract {
         using transfer_action = eosio::action_wrapper<"transfer"_n, &token::transfer>;
         using open_action = eosio::action_wrapper<"open"_n, &token::open>;
         using close_action = eosio::action_wrapper<"close"_n, &token::close>;
-        using setissuer_action = eosio::action_wrapper<"setissuer"_n, &token::setissuer>;
     private:
         struct [[eosio::table]] account {
             asset    balance;
